8ec9f97f7a3669991abf4b01ad91905544e916b5
Pablo Boing Pscheidt <pablo.pscheidt@magazord.com.br>
2022-06-03 08:56:35 -0300

    Feito parte do input para pesquisar

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $( document ).ready( function() {
     "use strict";
 
+    //Fazer aparecer as informações no click
     $('.menu-informations').on('click', function() {
         $('.menu-informations .arrow-down').toggleClass('active');
         $('.informations').toggleClass('show-informations');
     });
-
+    //Fazer a troca entre a parte dos repositorios e das estrelas
     $('.repositories').on('click', function() {
         $('.content-repositories').addClass('show-content');
         $('.content-starreds').removeClass('show-content');
@@ -19,7 +20,7 @@ $( document ).ready( function() {
         $('.repositories').removeClass('show-top-content');
         $('.starred').addClass('show-top-content');
     });
-
+    //Abrir e fechar os filtros
     $('.type-filter').on('click', function() {
         $('.filter-type').addClass('open-filter');
         $('html').addClass('over-active');
@@ -41,4 +42,23 @@ $( document ).ready( function() {
         $('.filter-language').removeClass('open-filter');
         $('html').removeClass('over-active');
     });
+
+    $(document).keyup(function(e) { 
+        if (e.keyCode === 27) 
+            $('.filter-language').removeClass('open-filter');
+            $('.filter-type').removeClass('open-filter');
+            $('html').removeClass('over-active');
+    }); 
+    //abrir a parte de pesquisa e retirar os filtros
+    $('#input-search').focus(function(){
+        $(this).attr('placeholder','Type something here...')
+        $('.filter-content').css('display','none');
+        $('.search-area').css('flex-direction','row-reverse')
+    });
+    $('#input-search').focusout(function(){
+        $(this).removeAttr('placeholder','Type something here...')
+        $('.filter-content').css('display','flex');
+        $('.search-area').css('flex-direction','row')
+    });
+
 })
